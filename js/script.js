7d5e18d1d70cf45394ebda4b6742f5f98f232c72
Gintasp <gplonis@gmail.com>
2018-05-23 15:46:47 +0300

    Finalize menu functionality

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+//toggle menu on small screens
 $('.burger').on('click',function(){
     $('.menu-small').toggleClass('visible');
     $('.hero-heading').toggleClass('absolute');
-})
+});
+
+//define menu scrolling function;
+function scroll(from, to){
+    $(from).on('click',function(){
+        $('html, body').animate({
+            scrollTop: $(to).offset().top
+        })
+    })
+};
+
+//scroll to elements
+scroll('#about','.about');
+scroll('#services','.service');
+scroll('#projects','.projects');
+scroll('#testimonials','.testimonial');
+scroll('#team','.team');
+scroll('#blog','.blog');
+scroll('#contacts','.contact');
