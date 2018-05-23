62076a622893def2f33269c459f815b4f6ed028d
Gintasp <gplonis@gmail.com>
2018-05-23 18:17:22 +0300

    Add back to top button

--- js/script.js
+++ js/script.js
@@ -13,6 +13,14 @@ function scroll(from, to){
     })
 };
 
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset>document.querySelector('.slider').clientHeight){
+        $('.up').addClass('visible-up');
+    }else{
+        $('.up').removeClass('visible-up');
+    }
+});
+
 //scroll to elements
 scroll('#about, #about-small','.about');
 scroll('#services, #services-small','.service');
@@ -21,3 +29,4 @@ scroll('#testimonials, #testimonials-small','.testimonial');
 scroll('#team, #team-small','.team');
 scroll('#blog, #blog-small','.blog');
 scroll('#contacts, #contact-small','.contact');
+scroll('.up','html, body');
