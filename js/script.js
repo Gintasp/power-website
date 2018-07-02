14ed1d7c3a589057a2c4e454cb95496d8c54145a
Gintasp <gplonis@gmail.com>
2018-07-02 11:29:25 +0300

    Prettify

--- js/script.js
+++ js/script.js
@@ -1,32 +1,31 @@
 //toggle menu on small screens
-$('.burger').on('click',function(){
-    $('.menu-small').toggleClass('visible');
-    $('.hero-heading').toggleClass('absolute');
+$('.burger').on('click', function() {
+  $('.menu-small').toggleClass('visible');
+  $('.hero-heading').toggleClass('absolute');
 });
 
-//define menu scrolling function;
-function scroll(from, to){
-    $(from).on('click',function(){
-        $('html, body').animate({
-            scrollTop: $(to).offset().top
-        })
-    })
-};
+function scroll(from, to) {
+  $(from).on('click', function() {
+    $('html, body').animate({
+      scrollTop: $(to).offset().top,
+    });
+  });
+}
 
-window.addEventListener('scroll',function(){
-    if(window.pageYOffset>document.querySelector('.slider').clientHeight){
-        $('.up').addClass('visible-up');
-    }else{
-        $('.up').removeClass('visible-up');
-    }
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > document.querySelector('.slider').clientHeight) {
+    $('.up').addClass('visible-up');
+  } else {
+    $('.up').removeClass('visible-up');
+  }
 });
 
 //scroll to elements
-scroll('#about, #about-small','.about');
-scroll('#services, #services-small','.service');
-scroll('#projects, #projects-small','.projects');
-scroll('#testimonials, #testimonials-small','.testimonial');
-scroll('#team, #team-small','.team');
-scroll('#blog, #blog-small','.blog');
-scroll('#contacts, #contact-small','.contact');
-scroll('.up','html, body');
+scroll('#about, #about-small', '.about');
+scroll('#services, #services-small', '.service');
+scroll('#projects, #projects-small', '.projects');
+scroll('#testimonials, #testimonials-small', '.testimonial');
+scroll('#team, #team-small', '.team');
+scroll('#blog, #blog-small', '.blog');
+scroll('#contacts, #contact-small', '.contact');
+scroll('.up', 'html, body');
